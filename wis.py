bb6e7c21d26822c015ffc7968ee649b1ef239849
pniaczek <62545956+pniaczek@users.noreply.github.com>
2020-04-19 14:51:39 +0200

    skonczona gra w wisielca

--- wis.py
+++ wis.py
@@ -1,7 +1,38 @@
-
 import random
-import sys
 
 
+lista_slow = ['banan', 'ananas', 'arbuz', 'mandarynka']
 
-print("Hello World")
+
+slowo = lista_slow[random.randrange(0, len(lista_slow))]
+print('Słowo które musisz zgadnąć ma', len(slowo),'liter')
+
+i = 0
+
+zuzyte_litery = []
+
+while (True):
+    m = 0
+
+    litera = input("\npodaj jedną literę: ")
+
+    for n in range(0, len(zuzyte_litery)):
+        if litera == zuzyte_litery[n]:
+            m = 1
+
+
+    if slowo.find(litera) != -1 and m == 0:
+
+        zuzyte_litery.append(litera)
+
+        for p in range(0, len(slowo)):
+            if litera == slowo[p]:
+                m = m+1
+
+        print("liter", litera, "jest", m, "w zgadywanym słowie")
+        i = i+m
+
+    print("zgadłeś już", i, "z", len(slowo), "liter w słowie")
+
+    if i == len(slowo):
+        break
